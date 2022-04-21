76f23a223d63d89941e4b5482b00da79bdf4a596
Sudad Al-Ajili <sudad.iq@gmail.com>
2022-04-21 23:52:00 +0300

    Deleting Posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,9 +6,13 @@ from posts.models import Post, Vote
 class PostSerializer(serializers.ModelSerializer):
     poster = serializers.ReadOnlyField(source='poster.username')
     poster_id = serializers.ReadOnlyField(source='poster.id')
+    votes = serializers.SerializerMethodField()
     class Meta:
         model: Post = Post
-        fields: list = ['id', 'created', 'poster', 'poster_id', 'title', 'url']
+        fields: list = ['id', 'created', 'poster', 'poster_id', 'title', 'url', 'votes']
+
+    def get_votes(self, post):
+        return Vote.objects.filter(post=post).count()
 
 
 class VoteSerializer(serializers.ModelSerializer):
--- zappit/urls.py
+++ zappit/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from posts import views as post_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/posts', post_views.PostList.as_view()),
+    path('api/posts/<int:pk>', post_views.PostRetrieveDestroy.as_view()),
     path('api/posts/<int:pk>/vote', post_views.VoteCreate.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
 ]
